8be21f5a15ab607e2f08675a6756382b74d7533c
Charlie Dean <charlie.dean888@gmail.com>
2024-12-03 07:15:26 +0000

    Finish day 2

--- 2.py
+++ 2.py
@@ -1,3 +1,8 @@
+part_1 = 0
+part_2 = 0
+
+# Part 1
+
 with open("input.txt") as f:
     reports = [i.strip().split(" ") for i in f]
 
@@ -7,26 +12,7 @@ def is_positive(n):
     else:
         return False
 
-sum = 0
-
-# Part 1
-#
-# for i, report in enumerate(reports):
-#     problems = 0
-#     report = [int(level) for level in report]
-#     diff = report[1]-report[0]
-#     trend = is_positive(diff)
-#     for j, level in enumerate(report[:-1]):
-#         diff = report[j+1]-report[j]
-#         trend_2 = is_positive(diff)
-#         if abs(diff) < 1 or abs(diff) > 3 or trend != trend_2:
-#             break
-#         if j == len(report[:-1])-1:
-#             sum += 1
-
-# Part 2
 for i, report in enumerate(reports):
-    problems = 0
     report = [int(level) for level in report]
     diff = report[1]-report[0]
     trend = is_positive(diff)
@@ -36,6 +22,41 @@ for i, report in enumerate(reports):
         if abs(diff) < 1 or abs(diff) > 3 or trend != trend_2:
             break
         if j == len(report[:-1])-1:
-            sum += 1
+            part_1 += 1
 
-print(sum)
+
+
+# Part 2
+
+with open("input.txt") as f:
+    reports = [i.strip().split(" ") for i in f]
+
+def check_if_safe(report):
+    diff = report[1]-report[0]
+    trend = is_positive(diff)
+    for j, level in enumerate(report[:-1]):
+        diff = report[j+1]-report[j]
+        trend_2 = is_positive(diff)
+        if abs(diff) < 1 or abs(diff) > 3 or trend != trend_2:
+            return False
+        if j == len(report[:-1])-1:
+            return True
+
+def problem_dampener(report):
+    for i, level in enumerate(report):
+        report_modified = report[:]
+        report_modified.pop(i)
+        if check_if_safe(report_modified):
+            return True
+    return False
+        
+for i, report in enumerate(reports):
+    report = [int(level) for level in report]
+    if check_if_safe(report):
+        part_2 += 1
+    else:
+        if problem_dampener(report):
+            part_2 += 1
+
+print(f"{part_1=}")
+print(f"{part_2=}")
