b8634241afc11cd6cfef28c265e0f60bfac86503
Charlie Dean <charlie.dean888@gmail.com>
2024-12-05 08:43:26 +0000

    Finish day 4

--- 4.py
+++ 4.py
@@ -42,3 +42,48 @@ for y, line in enumerate(input):
             part_1 += initiate_search(x, y)
 
 print(f"{part_1=}")
+
+
+# Part 2
+
+with open("input.txt") as f:
+    input = [line.strip("\n") for line in f]
+
+def get_letter(x, y):
+    if x < 0 or y < 0:
+        return None
+    if x > len(input[0])-1 or y > len(input)-1:
+        return None
+    return input[y][x]
+
+def initiate_search(x, y):
+    count = 0
+    for dx in range(-1, 2, 2):
+        for dy in range(-1, 2, 2):
+            target_character = search_string[0]
+            next_character = get_letter(x+dx, y+dy)
+            if next_character == target_character:
+                if continue_search(x, y, dx, dy):
+                    count += 1
+    if count == 2:
+        return True
+    else:
+        return False
+
+def continue_search(x, y, dx, dy):
+    target_character = search_string[2]
+    next_character = get_letter(x-dx, y-dy)
+    if next_character == target_character:
+        return True
+
+search_string = "MAS"
+
+part_2 = 0
+
+for y, line in enumerate(input):
+    for x, letter in enumerate(line):
+        if letter == search_string[len(search_string) // 2]:
+            if initiate_search(x, y):
+                part_2 += 1
+
+print(f"{part_2=}")
